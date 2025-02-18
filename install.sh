994b7abf2144874f4e5d3956729fd836962deeb7
Bard-Gaming <christophe.dronne@epitech.eu>
2025-02-18 12:49:03 +0100

    [Install] Updated install script

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ install_custom_scripts() {
     if [[ -d "$DESTINATION" ]]; then
         local OLD_DEST
 
-        OLD_DEST="$HOME/old_custom_scripts"
+        OLD_DEST="$HOME/.custom_scripts_old"
         echo "$SCRIPT_NAME: Moved already existing custom scripts dir to $OLD_DEST"
         mv "$DESTINATION" "$OLD_DEST"
     fi
@@ -28,19 +28,30 @@ install_custom_scripts() {
     cp -r "$SCRIPTS_DIR" "$DESTINATION"
 }
 
-install_bashrc() {
-    local BASHRC_PATH
+install_file() {
+    local LOCAL_FILE_PATH
 
-    BASHRC_PATH="$FILE_DIR/.bashrc"
-
-    if [[ -f "$HOME/.bashrc" ]]; then
-        echo "$SCRIPT_NAME: Moved already existing bashrc file to $HOME/old_bashrc"
-        mv "$HOME/.bashrc" "$HOME/old_bashrc"
+    if [[ $# -lt 1 ]]; then
+        echo "$SCRIPT_NAME: Programmer error: install_file requires at least one arg"
+        exit 1
     fi
 
-    cp "$BASHRC_PATH" "$HOME"
+    LOCAL_FILE_PATH="$FILE_DIR/$1"
+    DEST_FILE_PATH="$HOME/$1"
+
+    if [[ -f "$DEST_FILE_PATH" ]]; then
+        local OLD_FILE_PATH
+
+        OLD_FILE_PATH="${DEST_FILE_PATH}_old"
+        echo "$SCRIPT_NAME: Moved already existing $1 file to $OLD_FILE_PATH"
+        mv "$DEST_FILE_PATH" "$OLD_FILE_PATH"
+    fi
+
+    cp "$LOCAL_FILE_PATH" "$DEST_FILE_PATH"
 }
 
 
-install_bashrc || exit 1
+install_file ".bashrc" || exit 1
 install_custom_scripts || exit 1
+install_file ".bash_aliases" || exit 1
+install_file ".bash_bookmarks" || exit 1
